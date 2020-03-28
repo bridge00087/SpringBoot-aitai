dc4fea52da30dfed1d7b523aa19e0a71dea2dcb7
s-kim <song2006ap@icloud.com>
2020-03-28 19:20:29 +0900

    プロジェクト生成

---
+++ src/test/java/com/aitai/AitaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
