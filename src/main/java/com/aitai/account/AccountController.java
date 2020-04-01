1e4ce3af5d79ca8caedd5b141d49a1e135365674
s-kim <song2006ap@icloud.com>
2020-04-01 22:18:12 +0900

    会員登録フォームサブミット処理

--- src/main/java/com/aitai/account/AccountController.java
+++ src/main/java/com/aitai/account/AccountController.java
@@ -1,6 +1,9 @@
 package com.aitai.account;
 
+import com.aitai.domain.Account;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,6 +19,8 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -34,6 +39,25 @@ public class AccountController {
         if (errors.hasErrors()) {
             return "account/sign-up";
         }
+
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) //TODO パスワードエンコディング必要
+                .meetingCreatedByWeb(true)
+                .meetingEnrollmentResultByWeb(true)
+                .meetingUpdatedByWeb(true)
+                .build();
+        Account newAccount = accountRepository.save(account);
+
+        newAccount.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("アイタイ, 会員登録認証");
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +
+                "&email=" + newAccount.getEmail());
+
+        javaMailSender.send(mailMessage);
 
         // 会員登録処理
         return "redirect:/";
